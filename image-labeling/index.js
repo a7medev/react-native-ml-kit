2dbc23e20baebc38e7e8c2be6953a43b0f57f655
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2021-10-16 08:29:18 +0200

    chore(image-labeling): add linking check

--- image-labeling/index.js
+++ image-labeling/index.js
@@ -1,7 +1,20 @@
-// main index.js
-
 import { NativeModules } from 'react-native';
 
-const { ImageLabeling } = NativeModules;
+const LINKING_ERROR =
+  `The package '@react-native-ml-kit/image-labeling' doesn't seem to be linked. Make sure: \n\n` +
+  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  '- You rebuilt the app after installing the package\n' +
+  '- You are not using Expo managed workflow\n';
+
+const ImageLabeling = NativeModules.ImageLabeling
+  ? NativeModules.ImageLabeling
+  : new Proxy(
+      {},
+      {
+        get() {
+          throw new Error(LINKING_ERROR);
+        },
+      }
+    );
 
 export default ImageLabeling;
