36f15b0f8aec682bda441387031f4082b8bd6604
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2021-10-16 21:55:42 +0200

    feat(identify-languages): android identify

--- identify-languages/android/src/main/java/com/rnmlkit/identifylanguages/IdentifyLanguagesModule.java
+++ identify-languages/android/src/main/java/com/rnmlkit/identifylanguages/IdentifyLanguagesModule.java
@@ -2,10 +2,17 @@
 
 package com.rnmlkit.identifylanguages;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.mlkit.nl.languageid.LanguageIdentification;
+import com.google.mlkit.nl.languageid.LanguageIdentifier;
 
 public class IdentifyLanguagesModule extends ReactContextBaseJavaModule {
 
@@ -22,8 +29,24 @@ public class IdentifyLanguagesModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
-        // TODO: Implement some actually useful functionality
-        callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
+    public void identify(String text, final Promise promise) {
+        LanguageIdentifier languageIdentifier =
+                LanguageIdentification.getClient();
+        languageIdentifier.identifyLanguage(text)
+                .addOnSuccessListener(
+                        new OnSuccessListener<String>() {
+                            @Override
+                            public void onSuccess(@Nullable String languageCode) {
+                                promise.resolve(languageCode);
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                e.printStackTrace();
+                                promise.reject("Language identification failed", e);
+                            }
+                        });
     }
 }
--- identify-languages/index.js
+++ identify-languages/index.js
@@ -1,7 +1,20 @@
-// main index.js
-
 import { NativeModules } from 'react-native';
 
-const { IdentifyLanguages } = NativeModules;
+const LINKING_ERROR =
+  `The package '@react-native-ml-kit/identify-languages' doesn't seem to be linked. Make sure: \n\n` +
+  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  '- You rebuilt the app after installing the package\n' +
+  '- You are not using Expo managed workflow\n';
 
-export default IdentifyLanguages;
+const IdentifyLanguage = NativeModules.IdentifyLanguages
+  ? NativeModules.IdentifyLanguages
+  : new Proxy(
+      {},
+      {
+        get() {
+          throw new Error(LINKING_ERROR);
+        },
+      }
+    );
+
+export default IdentifyLanguage;
