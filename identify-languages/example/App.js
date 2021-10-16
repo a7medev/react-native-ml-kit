d6616d027e7cec7810df4c527e1f144f3fcb5cfc
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2021-10-16 22:09:34 +0200

    chore(identify-language): add example code

--- identify-languages/example/App.js
+++ identify-languages/example/App.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
+import IdentifyLanguages from '@react-native-ml-kit/identify-languages';
 
 const App = () => {
+  const [text, setText] = useState('');
+  const [lang, setLang] = useState('');
+
+  useEffect(() => {
+    IdentifyLanguages.identify(text).then(setLang).catch(console.error);
+  }, [text]);
+
   return (
     <View style={styles.container}>
-      <Text>TODO: Add example</Text>
+      <Text style={styles.heading}>Enter text to detect language</Text>
+      <TextInput
+        placeholder="Enter text here"
+        multiline
+        textAlignVertical="top"
+        style={styles.input}
+        value={text}
+        onChangeText={_text => setText(_text)}
+      />
+      {lang ? (
+        <Text style={styles.heading}>Language Code is: {lang}</Text>
+      ) : null}
     </View>
   );
 };
@@ -14,6 +33,20 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  heading: {
+    fontSize: 20,
+    marginBottom: 10,
+    marginTop: 20,
+  },
+  input: {
+    height: 100,
+    width: '90%',
+    padding: 15,
+    backgroundColor: '#eee',
+    borderColor: '#ddd',
+    borderWidth: 1,
+    borderRadius: 5,
   },
 });
 
