140720958a4e883392328e6b27146f035e8433a6
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2021-10-16 22:53:48 +0200

    feat(identify-languages): ios identify possible

--- identify-languages/ios/IdentifyLanguages.m
+++ identify-languages/ios/IdentifyLanguages.m
@@ -1,6 +1,6 @@
 #import "IdentifyLanguages.h"
 
-@import MLKitLanguageID.MLKLanguageIdentification;
+@import MLKitLanguageID;
 
 @implementation IdentifyLanguages
 
@@ -20,6 +20,33 @@ RCT_EXPORT_METHOD(identify: (nonnull NSString*)text
             return;
         }
         resolve(languageCode);
+    }];
+}
+
+RCT_EXPORT_METHOD(identifyPossible: (nonnull NSString*)text
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+{
+    MLKLanguageIdentification *languageId = [MLKLanguageIdentification languageIdentification];
+    
+    [languageId identifyPossibleLanguagesForText:text
+                                      completion:^(NSArray * _Nonnull identifiedLanguages,
+                                                   NSError * _Nullable error) {
+        if (error != nil) {
+            reject(@"Identify Languages", @"Language identification failed", error);
+            return;
+        }
+        
+        NSMutableArray *result = [NSMutableArray array];
+
+        for (MLKIdentifiedLanguage *language in identifiedLanguages) {
+            [result addObject:@{
+                @"language": language.languageTag,
+                @"confidence": @(language.confidence)
+            }];
+        }
+
+        resolve(result);
     }];
 }
 
