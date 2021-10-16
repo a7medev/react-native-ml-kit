6cab9a6412bd7b3bf8ac1838a39168b85d90b3bd
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2021-10-16 22:23:10 +0200

    chore(image-labeling): add jsdoc comments

--- image-labeling/index.d.ts
+++ image-labeling/index.d.ts
@@ -5,6 +5,12 @@ export interface Label {
 }
 
 interface IImageLabeling {
+  /**
+   * Label an image
+   *
+   * @param imageURL The URL/Path of the image to label
+   * @returns Array of expected labels for the image
+   */
   label: (imageURL: string) => Promise<Label[]>;
 }
 
