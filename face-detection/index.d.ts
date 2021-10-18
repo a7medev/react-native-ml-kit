6a378edcc3eca1e8644c60cf802fa9cdba1fbf73
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2021-10-18 08:39:32 +0200

    feat(face-detection): improve types & add jsdoc comments

--- face-detection/index.d.ts
+++ face-detection/index.d.ts
@@ -13,7 +13,9 @@ export interface Landmark {
   position: Point;
 }
 
-export type Coutour = Point[];
+export interface Coutour {
+  points: Point[];
+}
 
 export interface Face {
   frame: Frame;
@@ -32,7 +34,7 @@ export interface Face {
     mouthRight?: Landmark;
     mouthBottom?: Landmark;
   };
-  contours: {
+  contours?: {
     face?: Coutour;
     leftEye?: Coutour;
     rightEye?: Coutour;
@@ -56,16 +58,53 @@ export interface Face {
 }
 
 export interface FaceDetectionOptions {
-  performanceMode?: 'none' | 'all';
+  /**
+   * Favor speed or accuracy when detecting faces.
+   *
+   * @default 'fast'
+   */
+  performanceMode?: 'fast' | 'accurate';
+
+  /**
+   * Whether to attempt to identify facial "landmarks": eyes, ears, nose, cheeks, mouth, and so on.
+   *
+   * @default 'none'
+   */
   landmarkMode?: 'none' | 'all';
+
+  /**
+   * Whether to detect the contours of facial features. Contours are detected for only the most prominent face in an image.
+   *
+   * @default 'none'
+   */
   contourMode?: 'none' | 'all';
+
+  /**
+   * Whether or not to classify faces into categories such as "smiling", and "eyes open".
+   *
+   * @default 'none'
+   */
   classificationMode?: 'none' | 'all';
+
+  /**
+   * Sets the smallest desired face size, expressed as the ratio of the width of the head to width of the image.
+   *
+   * @default 0.1
+   */
   minFaceSize?: number;
+
+  /**
+   * Whether or not to assign faces an ID, which can be used to track faces across images.
+   *
+   * Note that when contour detection is enabled, only one face is detected, so face tracking doesn't produce useful results. For this reason, and to improve detection speed, don't enable both contour detection and face tracking.
+   *
+   * @default false
+   */
   trackingEnabled?: boolean;
 }
 
 interface IFaceDetection {
-  detect: (imageURL: string, options: FaceDetectionOptions) => Promise<Face[]>;
+  detect: (imageURL: string, options?: FaceDetectionOptions) => Promise<Face[]>;
 }
 
 declare const FaceDetection: IFaceDetection;
