76b4207668c25d480c1912383c9115dc2f089039
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2022-05-15 19:43:14 +0200

    chore(barcode): add TypeScript support

---
+++ barcode-scanning/index.d.ts
@@ -0,0 +1,30 @@
+export enum BarcodeFormat {
+  UNKNOWN = -1,
+  ALL_FORMATS = 0,
+  CODE_128 = 1,
+  CODE_39 = 2,
+  CODE_93 = 4,
+  CODABAR = 8,
+  DATA_MATRIX = 16,
+  EAN_13 = 32,
+  EAN_8 = 64,
+  ITF = 128,
+  QR_CODE = 256,
+  UPC_A = 512,
+  UPC_E = 1024,
+  PDF417 = 2048,
+  AZTEC = 4096,
+}
+
+interface Barcode {
+  format: BarcodeFormat;
+  value: string;
+}
+
+interface IBarcodeScanning {
+  scan: (imageURL: string) => Promise<Barcode[]>;
+}
+
+declare const BarcodeScanning: IBarcodeScanning;
+
+export default BarcodeScanning;
--- barcode-scanning/index.js
+++ barcode-scanning/index.js
@@ -1,10 +1,28 @@
-import { NativeModules } from "react-native";
+import { NativeModules } from 'react-native';
 
 const LINKING_ERROR =
   `The package '@react-native-ml-kit/barcode-scanning' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: "" }) +
-  "- You rebuilt the app after installing the package\n" +
-  "- You are not using Expo managed workflow\n";
+  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  '- You rebuilt the app after installing the package\n' +
+  '- You are not using Expo managed workflow\n';
+
+export const BarcodeFormat = Object.freeze({
+  UNKNOWN: -1,
+  ALL_FORMATS: 0,
+  CODE_128: 1,
+  CODE_39: 2,
+  CODE_93: 4,
+  CODABAR: 8,
+  DATA_MATRIX: 16,
+  EAN_13: 32,
+  EAN_8: 64,
+  ITF: 128,
+  QR_CODE: 256,
+  UPC_A: 512,
+  UPC_E: 1024,
+  PDF417: 2048,
+  AZTEC: 4096,
+});
 
 const BarcodeScanning = NativeModules.BarcodeScanning
   ? NativeModules.BarcodeScanning
