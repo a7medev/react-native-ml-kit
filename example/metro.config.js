dbe5a03521f7d3452704aa83af358c4de2974b82
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2023-09-18 20:08:43 +0300

    chore: integrate expo modules

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,4 +1,5 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const {getDefaultConfig} = require('expo/metro-config');
+const {mergeConfig} = require('@react-native/metro-config');
 
 /** @type {import('metro-config').MetroConfig} */
 const config = {};
