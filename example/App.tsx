93cfb7a2a49d5e3ef4d8f6ff954f32148e3b59b9
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2023-09-18 18:55:45 +0300

    chore: init example app

---
+++ example/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <View>
+      <Text>Hello React Native ML Kit!</Text>
+    </View>
+  );
+};
+
+export default App;
---
+++ example/metro.config.js
@@ -0,0 +1,6 @@
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
+/** @type {import('metro-config').MetroConfig} */
+const config = {};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
