2373c49c362dd6f17a8a94243f058787588bcbe8
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2023-09-30 16:45:39 +0300

    chore(image-labeling): publish v1.4.0 🚀

--- image-labeling/index.ts
+++ image-labeling/index.ts
@@ -1,12 +1,28 @@
 import { NativeModules, Platform } from 'react-native';
 
+export interface Label {
+  text: string;
+  confidence: number;
+  index: number;
+}
+
+interface IImageLabeling {
+  /**
+   * Label an image
+   *
+   * @param imageURL The URL/Path of the image to label
+   * @returns Array of expected labels for the image
+   */
+  label: (imageURL: string) => Promise<Label[]>;
+}
+
 const LINKING_ERROR =
   `The package '@react-native-ml-kit/image-labeling' doesn't seem to be linked. Make sure: \n\n` +
   Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
   '- You rebuilt the app after installing the package\n' +
   '- You are not using Expo managed workflow\n';
 
-const ImageLabeling = NativeModules.ImageLabeling
+const ImageLabeling: IImageLabeling = NativeModules.ImageLabeling
   ? NativeModules.ImageLabeling
   : new Proxy(
       {},
